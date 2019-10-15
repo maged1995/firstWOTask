92159ec6170518dc6d314a29579c738c7b72b971
maged1995 <magedsaadaziz@hotmail.com>
2019-10-15 23:23:13 +0200

    Removed extra Tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :coaches
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :authors, only: [:create]
   resources :comments, only: [:create, :update, :destroy, :show]
